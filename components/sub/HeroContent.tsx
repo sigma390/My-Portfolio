51edd4631911641e59475a5e334c72d7e4f4326a
Omkar Patil <git21cs072-t@students.git.edu>
2023-12-01 14:54:29 +0530

    hero CONTENT Section

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import {motion} from 'framer-motion'
 export const HeroContent = () => {
   return (
-    <div>HeroContent</div>
+    <motion.div
+    initial = "hidden"
+    animate = "visible"
+    className='flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]'>
+      <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
+        <motion.div
+        className='Welcom-box py-[15px] px-[4px] border border-[#7042f88b] opacity-[0.9]'>
+
+
+        </motion.div>
+
+
+      </div>
+      </motion.div>
   )
 }
