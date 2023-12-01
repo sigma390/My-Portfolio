4315548ffe0dfa8489bbac5c7d3e59928fd19dfe
Omkar Patil <git21cs072-t@students.git.edu>
2023-12-01 15:03:44 +0530

    Global css Populated

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {motion} from 'framer-motion'
+import { slideInFromTop } from '@/utils/motion'
+import { SparklesIcon } from '@heroicons/react/24/outline'
 export const HeroContent = () => {
   return (
     <motion.div
@@ -11,7 +13,8 @@ export const HeroContent = () => {
         variants={slideInFromTop}
         className='Welcom-box py-[15px] px-[4px] border border-[#7042f88b] opacity-[0.9]'>
 
-
+        <SparklesIcon className='text -[#b4pbff] mr-[10px] h-5 w-5' />
+        <h1 className='Welcom-text text-[13px]'>FullStack Developer Portfolio</h1>
         </motion.div>
 
 
