26f0a1953dd622a0c015cd55175b18004a50bdff
Omkar Patil <git21cs072-t@students.git.edu>
2023-12-06 20:24:52 +0530

    Section Setted up fully

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -1,7 +1,12 @@
 import React,{useState, useRef ,Suspense} from 'react'
-
-export default function StarBackground() {
+import {Canvas,useFrame} from '@react-three/fiber'
+import { Points, PointMaterial, Preload } from '@react-three/drei'
+//@ts-ignore
+import * as random from 'maath/random/dist/maath-random.esm'
+const StarBackground = (props:any)=> {
   return (
     <div>StarBackground</div>
   )
 }
+
+export default StarBackground;
