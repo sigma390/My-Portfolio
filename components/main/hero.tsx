6dd083f285414b44139bfb67ba20357f7ca174d4
Omkar Patil <git21cs072-t@students.git.edu>
2023-12-06 20:11:53 +0530

    Modifications

--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { HeroContent } from '../sub/HeroContent'
 
 export const Hero = () => {
   return (
@@ -7,11 +8,12 @@ export const Hero = () => {
         autoPlay
         muted
         loop
-        className='rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover '
+        className='rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover'
         >
             <source src='/blackhole.webm' type='video/webm' />
 
         </video>
+        <HeroContent/>
 
 
     </div>
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -2,8 +2,9 @@
 'use client'
 import React from 'react'
 import {motion} from 'framer-motion'
-import { slideInFromLeft, slideInFromTop } from '@/utils/motion'
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion'
 import { SparklesIcon } from '@heroicons/react/24/outline'
+import Image from 'next/image';
 export const HeroContent = () => {
   return (
     <motion.div
@@ -13,16 +14,20 @@ export const HeroContent = () => {
       <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
         <motion.div
         variants={slideInFromTop}
-        className='Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9]'>
+        className='Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]'>
 
         <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
         <h1 className='Welcome-text text-[13px]'>FullStack Developer Portfolio</h1>
         </motion.div>
         <motion.div 
         variants={slideInFromLeft(0.5)}
-        className='flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'>
-          Providing<span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'>the best</span>
-        project experience
+        className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'>
+          <span>
+          Providing
+          <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> the best </span>
+          project experience
+          </span>
+          
         </motion.div>
 
         <motion.p
@@ -38,6 +43,17 @@ export const HeroContent = () => {
 
 
       </div>
+      <motion.div
+      variants={slideInFromRight(0.8)}
+      className='w-full h-full flex justify-center items-center'>
+        <Image 
+        src="/mainIconsdark.svg"
+        alt='work icons'
+        height={650}
+        width={650}/>
+
+
+      </motion.div>
       </motion.div>
   )
 }
