ab03853363d902117bf3ff3651fa2f8a738e015f
Omkar Patil <git21cs072-t@students.git.edu>
2023-12-10 15:20:32 +0530

    Encryption Section done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import Encryption from '@/components/main/Encryption'
 import { Hero } from '@/components/main/Hero'
+import Skills from '@/components/main/Skills'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className='h-full w-full' >
-      <div className='flex flex-col h-[850px] gap-20'>
+      <div className='flex flex-col gap-20'>
         <Hero/>
+        <Skills/>
+        <Encryption/>
       </div>
     </main>
   )
--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -3,7 +3,7 @@ import { HeroContent } from '../sub/HeroContent'
 
 export const Hero = () => {
   return (
-    <div className='relative flex flex-col h-full w-full'>
+    <div className='relative flex flex-col h-full w-full' id="about-me">
         <video 
         autoPlay
         muted
